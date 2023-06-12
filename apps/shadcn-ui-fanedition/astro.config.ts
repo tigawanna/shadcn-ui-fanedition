5ce13bc08f6d8f48e0bc43b9a11b4ef3bae66aff
tigawanna <denniskinuthiaw@gamil.com>
2023-06-12 03:35:07 +0300

    added taiwindcss

--- apps/shadcn-ui-fanedition/astro.config.ts
+++ apps/shadcn-ui-fanedition/astro.config.ts
@@ -1,28 +1,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'ShadcnUI FE ',
-    social: {
-      github: 'https://github.com/tigawanna/shadcn-ui-fanedition'
-    },
-    sidebar: [{
-      label: 'Guides',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        link: '/guides/example/'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }]
-  }), tailwind()]
+  integrations: [
+    starlight({
+      title: 'ShadcnUI FE ',
+      social: {
+        github: 'https://github.com/tigawanna/shadcn-ui-fanedition',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: 'Example Guide',
+              link: '/guides/example/',
+            },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: {
+            directory: 'reference',
+          },
+        },
+      ],
+    }),
+    tailwind(),
+  ],
 });
