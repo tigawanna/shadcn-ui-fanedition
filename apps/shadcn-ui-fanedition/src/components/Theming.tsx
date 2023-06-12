135f05ba8267c834176673421593ec5e1bc7bc8a
tigawanna <denniskinuthiaw@gamil.com>
2023-06-12 16:37:19 +0300

    theming

--- apps/shadcn-ui-fanedition/src/components/Theming.tsx
+++ apps/shadcn-ui-fanedition/src/components/Theming.tsx
@@ -1,12 +1,5 @@
+interface ThemingProps {}
 
-interface ThemingProps {
-
-}
-
-export function Theming({}:ThemingProps){
-return (
- <div className='w-full h-full flex items-center justify-center'>
-        theming section
- </div>
-);
+export function Theming({}: ThemingProps) {
+  return <div className="w-full h-full flex items-center justify-center">theming section</div>;
 }
