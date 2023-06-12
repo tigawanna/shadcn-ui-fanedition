2616de49e050d82778e26d723b744d9927f411e0
tigawanna <denniskinuthiaw@gamil.com>
2023-06-12 16:11:41 +0300

    setup shadcn

--- apps/shadcn-ui-fanedition/src/components/info.tsx
+++ apps/shadcn-ui-fanedition/src/components/info.tsx
@@ -6,9 +6,8 @@ export function Info({}: infoProps) {
   return (
     <div
       //  style={{width:"100%", height:"100%" ,backgroundColor:"red"}}
-      className="w-full h-full flex items-center justify-center bg-purple-400"
-    >
-      <Button className="bg-red-800">hello</Button>
+      className="w-full h-full flex items-center justify-center">
+      <Button variant="ghost">hello</Button>
     </div>
   );
 }
