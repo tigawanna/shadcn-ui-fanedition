bd68b5da5fbfd27f710da9d319a48b17a987f5be
tigawanna <denniskinuthiaw@gamil.com>
2023-06-26 19:12:05 +0300

    new shadcn cli

---
+++ packages/cli/src/custom_commands/consts.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_STYLE = "default"
+export const DEFAULT_TAILWIND_CSS = "src/app/globals.css"
+export const DEFAULT_TAILWIND_CONFIG = "tailwind.config.js"
+export const DEFAULT_TAILWIND_BASE_COLOR = "slate"
+
+export const DEFAULT_BASE_DIR = "src/shadcn"
+export const DEFAULT_COMPONENTS = "src/shadcn/ui"
+export const DEFAULT_UTILS = "src/shadcn/lib/utils"
